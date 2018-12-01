a9b4447e94fbd4bc537e47d38a40fb452ff7eeaa
Liam Bowers <liamondrop@gmail.com>
2018-12-01 10:13:15 -0800

    send proportional messages to i2cpwm_board

--- src/turtle_teleop_joy.cpp
+++ src/turtle_teleop_joy.cpp
@@ -20,6 +20,7 @@ private:
     double l_scale_, a_scale_;
     ros::Publisher vel_pub_;
     ros::Subscriber joy_sub_;
+    ros::Publisher i2cpwm_pub_;
 };
 
 TeleopTurtle::TeleopTurtle():
@@ -30,6 +31,7 @@ TeleopTurtle::TeleopTurtle():
     nh_.param("scale_linear", a_scale_, a_scale_);
     nh_.param("scale_angular", l_scale_, l_scale_);
     vel_pub_ = nh_.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1);
+    i2cpwm_pub_ = nh_.advertise<i2cpwm_board::ServoArray>("servos_proportional", 1);
     joy_sub_ = nh_.subscribe<sensor_msgs::Joy>(
         "joy", 10, &TeleopTurtle::joyCallback, this);
 }
@@ -41,7 +43,17 @@ void TeleopTurtle::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
     twist.linear.x = l_scale_ * joy->axes[linear_];
     vel_pub_.publish(twist);
 
+    i2cpwm_board::Servo servo1;
+    servo1.servo = 1;
+    servo1.value = l_scale_ * joy->axes[linear_];
 
+    i2cpwm_board::Servo servo2;
+    servo2.servo = 2;
+    servo2.value = a_scale_ * joy->axes[angular_];
+
+    i2cpwm_board::ServoArray servo_array;
+    servo_array.servos = {servo1, servo2};
+    i2cpwm_pub_.publish(servo_array);
 }
 
 // rosservice call /config_drive_mode "{mode: ackerman, rpm: 56.0, radius: 0.038, track: 0.18, scale: 1.0, servos: [{servo: 1, position: 1}, {servo: 2, position: 1}]}"
