f8d527073e47f80f57620ce3ce9cd101750afb84
Liam Bowers <liamondrop@gmail.com>
2018-12-16 10:22:47 -0800

    refining actuator

--- donkeycar/parts/actuator/src/donkey_actuator/actuator.py
+++ donkeycar/parts/actuator/src/donkey_actuator/actuator.py
@@ -1,23 +1,28 @@
 import Adafruit_PCA9685
 import rospy
-from geometry_msgs.msg import Twist
+import time
+
 from donkey_actuator.msg import Servo
+from geometry_msgs.msg import Twist
 
 
-def map_value_to_pwm_(servo_config, value):
+def map_value_to_pulse_(servo, value):
     if value < -1.0001 or value > 1.0001:
         rospy.logerr('({}) value must be between -1.0 and 1.0'.format(value))
         return 0
-    pwm = servo_config.get('direction') * \
-        (0.5 * servo_config.get('range') * value) + servo_config.get('center')
-    return int(pwm)
+    pulse = servo['direction'] * 0.5 * servo['range'] * value + servo['center']
+    return int(pulse)
 
 
 class Actuator:
     def __init__(self):
         self.servos = rospy.get_param('servos')
-        self.pwm = Adafruit_PCA9685.PCA9685()
-        self.pwm.set_pwm_freq(self.servos.get('pwm_frequency'))
+        self.controller = Adafruit_PCA9685.PCA9685()
+        self.controller.set_pwm_freq(self.servos.get('pwm_frequency'))
+
+        # send center pulse to throttle servo to calibrate ESC
+        self.set_servo_center_(self.servos['throttle'])
+        time.sleep(1)
 
         # initiate subscribers
         rospy.Subscriber('servo_absolute', Servo, self.servo_absolute_cb_)
@@ -36,10 +41,7 @@ class Actuator:
         $ rostopic pub servo_absolute donkey_actuator/Servo "{name: steering, value: 335}"
         $ rostopic pub servo_absolute donkey_actuator/Servo "{name: steering, value: 333}"
         """
-        config = self.servos.get(msg.name)
-        self.pwm.set_pwm(config.get('channel'), 0, int(msg.value))
-        rospy.loginfo('servo: {}, channel: {}, value: {}'.format(
-                      msg.name, config.get('channel'), int(msg.value)))
+        self.set_servo_pulse_(self.servos[msg.name], int(msg.value))
 
     def servos_drive_cb_(self, msg):
         """
@@ -53,17 +55,25 @@ class Actuator:
         at 75% throttle:
         $ rostopic pub servos_drive geometry_msgs/Twist "{linear: {x: 0.75}, angular: {z: 0.0}}"
         """
-        self.set_servo_proportional_('steering', msg.angular.z)
-        self.set_servo_proportional_('throttle', msg.linear.x)
+        self.set_servo_proportional_(self.servos['steering'], msg.angular.z)
+        self.set_servo_proportional_(self.servos['throttle'], msg.linear.x)
 
-    def set_servo_proportional_(self, servo_name, value):
+    def set_servo_center_(self, servo):
+        self.set_servo_pulse_(servo, servo['center'])
+
+    def set_servo_pulse_(self, servo, value):
+        channel = servo['channel']
+        self.controller.set_pwm(channel, 0, value)
+        rospy.loginfo('servo: {}, channel: {}, value: {}'.format(
+            servo_name, channel, value))
+
+    def set_servo_proportional_(self, servo, value):
+        pulse = map_value_to_pwm_(servo, value)
+        self.set_servo_pulse_(servo, pulse)
+
+    def __del__(self):
         """
-        Function to set a given servo's pwm value based on a proportional
-        value between -1.0 and 1.0
+        Turn off servos when shutting down
         """
-        config = self.servos.get(servo_name)
-        pulse = map_value_to_pwm_(config, value)
-        self.pwm.set_pwm(config.get('channel'), 0, pulse)
-        rospy.loginfo(
-            'servo: {}, channel: {}, value: {}, pulse: {}'.format(
-            servo_name, config.get('channel'), value, pulse))
+        self.set_servo_pulse_(self.servos['steering'], 0)
+        self.set_servo_pulse_(self.servos['throttle'], 0)
